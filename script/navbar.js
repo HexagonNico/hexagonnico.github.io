42f6b73dc2f33074bd65739b7b1351691101b30b
Nico <nico.hex6@gmail.com>
2022-06-18 18:20:13 +0200

    Home page navbar

---
+++ script/navbar.js
@@ -0,0 +1,12 @@
+
+// Hide navbar on scroll
+var prevScrollPos = window.scrollY;
+window.onscroll = function() {
+	var currentScrollPos = window.scrollY;
+	if(prevScrollPos > currentScrollPos) {
+		document.getElementById("navbar").style.top = "0";
+	} else {
+		document.getElementById("navbar").style.top = "-50px";
+	}
+	prevScrollPos = currentScrollPos;
+}
